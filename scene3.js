c0ee7e34681b2a9a0a546016cbb44b180db1c9ed
Simon Petrikov <moran.phillipjan.itstaff@gmail.com>
2023-10-13 18:05:40 +0700

    try

--- scene3.js
+++ scene3.js
@@ -10,16 +10,13 @@ class Scene3 extends Phaser.Scene{
     this.background.setOrigin(0.25,0);
     this.fruits = this.add.group();
 
-    // this.time.addEvent({
-    //   delay: 1000, // Milliseconds between fruit spawns
-    //   callback: () => {
-    //     this.randomFruitImage = Phaser.Math.RND.pick(['fruit1', 'fruit2', 'fruit3']);
-    //     this.x = Phaser.Math.Between(0, config.width);
-    //     this.frut = this.fruits.create(this.x, 0, this.randomFruitImage);
-
-    //   },
-    //   loop: true,
-    // });
+    this.time.addEvent({
+      delay: 1000, // Milliseconds between fruit spawns
+      callback: () => {
+        this.createMovingImage();
+      },
+      loop: true,
+    });
     const fontSize = Math.min(config.width, config.height) * 0.07;
     this.scoreText = this.add.text(16, 16, 'Score: 0', {
       fontSize: fontSize + 'px',
@@ -27,39 +24,38 @@ class Scene3 extends Phaser.Scene{
     });
   }
   //update
-  // update(){
-  //   this.randomFruitImage = Phaser.Math.RND.pick(['fruit1', 'fruit2', 'fruit3']);
-  //   this.x = Phaser.Math.Between(0, config.width);
-  //   this.frut = this.fruits.create(this.x, 0, this.randomFruitImage);
-  //   // this.generateFruits()
-  // }
   update(){
-    if (game.getTime() >= nextFruitSpawnTime) {
-      this.randomFruitImage = Phaser.Math.RND.pick(['fruit1', 'fruit2', 'fruit3']);
-      this.x = Phaser.Math.Between(0, config.width);
-      this.fruit = this.fruits.create(this.x, 0, this.randomFruitImage);
-      // fruit.setVelocity(0, 100); // Fall speed (y+ direction)
-  
-      // Update the time for the next fruit spawn
-      nextFruitSpawnTime = game.getTime() + fruitSpawnDelay;
+    if(this.frut){
+      this.frut.y += this.generateRandomSpeed()
     }
   }
   updateScore(points) {
     score += points;
     this.scoreText.setText('Score: ' + score);
   }
-  generateFruits(){
-    
-    this.time.addEvent({
-      delay: 1000, // Milliseconds between fruit spawns
-      callback: () => {
-        this.randomFruitImage = Phaser.Math.RND.pick(['fruit1', 'fruit2', 'fruit3']);
-        this.x = Phaser.Math.Between(0, config.width);
-        this.fruit = this.fruits.create(this.x, 0, this.randomFruitImage);
-        this.moveship(this.fruit, this.generateRandomSpeed())
-      },
-      loop: true,
-    });
+  resetShipPos(ship){
+    ship.y = 0;
+    var centerWidth = config.width * 0.75; // 75% of config.width
+    var startPosition = (config.width - centerWidth) / 2; // Left-most position for the centered area
+    var randomX = Phaser.Math.Between(startPosition, startPosition + centerWidth);
+    ship.x = randomX;
+  }
+  //move
+  moveship(ship, speed){
+    ship.y+=speed;
+    if (ship.y > config.height){
+      this.resetShipPos(ship);
+    }
+  }
+  //generate
+  createMovingImage(){
+    this.randomFruitImage = Phaser.Math.RND.pick(['fruit1', 'fruit2', 'fruit3']);
+    var centerWidth = config.width * 0.75; // 75% of config.width
+    var startPosition = (config.width - centerWidth) / 2; // Left-most position for the centered area
+    var randomX = Phaser.Math.Between(startPosition, startPosition + centerWidth);
+    this.frut = this.fruits.create(randomX, 0, this.randomFruitImage);
+
+    this.moveship(this.frut, this.generateRandomSpeed())
   }
   resetShipPos(ship){
     ship.y = 0;
@@ -71,14 +67,7 @@ class Scene3 extends Phaser.Scene{
   //metrics
   generateRandomSpeed() {
     const minSpeed = 1; 
-    const maxSpeed = 4;
+    const maxSpeed = 10;
     return Phaser.Math.Between(minSpeed, maxSpeed);
-  }
-  moveship(ship, speed){
-    ship.y+=speed;
-    if (ship.y > config.height){
-      this.resetShipPos(ship);
-    }
-  }
-  
+  }  
 }
